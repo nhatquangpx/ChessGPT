0980029a45ebf146b9a282ba2b9da7b4fb5de428
nhatquangpx <doannhatquang0@gmail.com>
2025-03-31 23:08:29 +0700

    Board surface

---
+++ src/game.py
@@ -0,0 +1,20 @@
+import pygame
+
+from const import *
+
+class Game:
+    def __init__(self):
+        pass
+
+    #Show methods
+    def show_bg(self, surface):
+        for row in range(ROWS):
+            for col in range(COLS):
+                if (row + col) % 2 == 0:
+                    color = (234, 235, 200) # Light square color
+                else:
+                    color = (119, 154, 88) # Dark square color
+
+                rect = (col * SQSIZE, row * SQSIZE, SQSIZE, SQSIZE)
+                
+                pygame.draw.rect(surface, color, rect)
